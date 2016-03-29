b68f4b72d1ad867a37c69815931ba43c5c54d823
Luccan <luccan.ryanata@gmail.com>
2016-03-30 05:12:25 +0800

    Fixed Views, finishing basic submit functionality

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,33 @@
 class OrdersController < ApplicationController
 
   def new
-    @order = Order.new
+    @order_all = OrderAll.new
     @items = Item.all
+		@items.each do |item|
+			@order_all.orders.build(:item_id => item.id) #prepopulate item id
+		end
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.save
+		#flash[:notice] = "asdf"
+		#@test = params[:new_order_all][:orders_attributes]
+		#flash[:notice] = @test.to_str()
+    @order_all = OrderAll.new(order_all_params)
+		@orders = Array.new()
+		params[:order_all][:orders_attributes].each do |order|
+			puts 'DEBUGGING'
+			puts order
+			puts 'DEBUGGING'
+			puts order.item_id
+			puts order.quantity
+			puts 'END DEBUGGING'
+			if order[2] > ""
+				if order[:quantity].to_i() > 0
+					@orders.push(order)
+				end
+			end
+		end
+    if @order_all.save
       flash[:notice] = "Added new order"
       redirect_to root_path
     else
@@ -18,7 +38,7 @@ class OrdersController < ApplicationController
 
   private
 
-  def order_params
-    params.require(:order).permit(:delivery_time, :quantity, :item_id, :order_all_id)
+  def order_all_params
+    params.require(:order_all).permit(:delivery_time, orders_attributes: [ :item_id, :quantity ])
   end
 end
